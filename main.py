743d7db7b575b3938c6f2b942d5fc42c7223221a
Damian Cislak <cislak.d@gmail.com>
2020-11-06 21:11:00 +0100

    Add player class.

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 import pygame
+import random
 
-# Define some colors
-BLACK = (0, 0, 0)
+WIDTH = 1200
+HEIGHT = 900
+FPS = 60
+
+# define colors
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
 pygame.init()
-
-
-size = (900, 700)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("[ HnS Game ]")
-
-done = False
-
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('[HnS Game]')
 clock = pygame.time.Clock()
 
-while not done:
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.rect.x = 20
+        self.rect.y = 20
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.rect.x -= 5
+        if keystate[pygame.K_RIGHT]:
+            self.rect.x += 5
+        if keystate[pygame.K_UP]:
+            self.rect.y -= 5
+        if keystate[pygame.K_DOWN]:
+            self.rect.y += 5
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+running = True
+while running:
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-    screen.fill(BLACK)
-    pygame.display.flip()
-    clock.tick(60)
+            running = False
 
+    all_sprites.update()
+
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
 
 pygame.quit()
